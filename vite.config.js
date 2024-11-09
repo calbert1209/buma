98f413b6c1ea4ede5ac3ef91c5a0b03ac7d46500
Christian Albert <calbert1209@gmail.com>
2024-11-09 22:31:09 +0900

    configure vite dev server for secure context

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
-})
+  plugins: [
+    preact(),
+    /**
+     * @see {@link https://github.com/chaosprint/vite-plugin-cross-origin-isolation?tab=readme-ov-file#usage}
+     */
+    {
+      name: "configure-response-headers",
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          next();
+        });
+      },
+    },
+  ],
+  optimizeDeps: { exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"] },
+});
